14d6a8341b46932bcd0264e42c2f02d28f51fd20
Yugeta.Koji <geta1972@gmail.com>
2024-02-28 09:09:47 +0900

    enemy multi

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -2,42 +2,94 @@ import { Data }  from "./data.js"
 
 export class Enemy{
 	pattern_num = 0
+	enemys = []
+	max_count = 2
 
 	constructor(){
 		this.init()
 	}
 
+	// 表示サイズの割合
 	get rate(){
 		return Data.setting.chara.rate
 	}
 
+	// 新たに敵を出現させるかどうかの判定
+	get is_create(){
+		if(this.enemys.length >= this.max_count){
+			return false
+		}
+		if(this.enemys.length && this.enemys[this.enemys.length-1].pos.x > Data.canvas.width / this.max_count){
+			return false
+		}
+		return true
+	}
 
 	init(){
 		for(const enemy of Data.setting.enemy.items){
-			const data = Data.images.find(e => e.key === enemy.key)
-			enemy.img = data.data
-			enemy.w   = data.w
-			enemy.h   = data.h
+			for(const img_data of enemy.pattern){
+				const data = Data.images.find(e => e.key === img_data.key)
+				img_data.img = data.data
+				img_data.w   = data.w
+				img_data.h   = data.h
+			}
 		}
 	}
 
-	speed(){
-		return 1 / Data.setting.enemy.speed
+	random_enemy(){
+		return Math.floor(Math.random() * Data.setting.enemy.items.length)
+	}
+
+	random_pos(){
+		return {
+			x : Data.canvas.width,
+			y : Math.floor(Math.random() * Data.diff.height),
+		}
 	}
 
 	view(){
-		const d   = Data.setting.enemy.items[~~this.pattern_num]
-		const img = d.img
-		const x   = Data.canvas.width - 150
-		const y   = 100
-		const w   = d.w * this.rate
-		const h   = d.h * this.rate
+		if(this.is_create){
+			const data = {
+				num : this.random_enemy(),
+				pos : this.random_pos(),
+			}
+			this.enemys.push(data)
+		}
+		for(const d of this.enemys){
+			this.single_view(d)
+		}
+		this.clear()
+	}
+
+	single_view(data){
+		const item = Data.setting.enemy.items[data.num]
+		const d    = item.pattern[~~this.pattern_num]
+		const img  = d.img
+		const x    = data.pos.x
+		const y    = data.pos.y
+		const w    = d.w * this.rate
+		const h    = d.h * this.rate
 		Data.ctx.drawImage(img, x, y, w, h)
 
-		this.pattern_num += this.speed()
-		if(this.pattern_num >= Data.setting.enemy.items.length-1){
+		this.pattern_num += 1 / item.speed
+		if(this.pattern_num >= Data.setting.enemy.items[data.num].pattern.length-1){
 			this.pattern_num = 0
 		}
+
+		data.pos.x -= item.speed
 	}
+
+	clear(){
+		for(let i=this.enemys.length-1; i>=0; i--){
+			const data = this.enemys[i]
+			if(data.pos.x >= -Data.setting.enemy.items[data.num].pattern[~~this.pattern_num].w){continue}
+			this.enemys.splice(i,1)
+		}
+	}
+
+	collision(){
+
+	}
+
 
 }
